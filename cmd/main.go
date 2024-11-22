b737c433dc13b4fc045a02c6363920b42a980024
Tu Huynh <tu@keva.dev>
2024-11-23 02:28:33 +0800

    initial for the code gen

---
+++ cmd/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"path/filepath"
+
+	"github.com/tuhuynh27/go-ioc/ioc/generator"
+)
+
+func main() {
+	var (
+		dir    = flag.String("dir", ".", "Directory to scan for components")
+		output = flag.String("output", "wire/wire_gen.go", "Output file for generated code")
+	)
+	flag.Parse()
+
+	// Convert to absolute path
+	absDir, err := filepath.Abs(*dir)
+	if err != nil {
+		log.Fatalf("Error getting absolute path: %v", err)
+	}
+
+	log.Printf("Scanning directory: %s", absDir)
+
+	// Parse components
+	components, err := generator.ParseComponents(absDir)
+	if err != nil {
+		log.Fatalf("Error parsing components: %v", err)
+	}
+
+	log.Printf("Found %d components", len(components))
+	for _, comp := range components {
+		log.Printf("Component: %s (package: %s)", comp.Name, comp.Package)
+		log.Printf("- Qualifier: %s", comp.Qualifier)
+		log.Printf("- Implements: %v", comp.Implements)
+		log.Printf("- Dependencies: %v", comp.Dependencies)
+	}
+
+	// Generate code
+	gen := generator.NewGenerator(components)
+	if err := gen.Generate(absDir); err != nil {
+		log.Fatalf("Error generating code: %v", err)
+	}
+
+	log.Printf("Successfully generated wire file: %s", *output)
+}
--- examples/ioc-example-simple/main.go
+++ examples/ioc-example-simple/main.go
@@ -1,26 +1,16 @@
 package main
 
 import (
-	"github.com/tuhuynh27/go-ioc/examples/ioc-example-simple/logger"
-	"github.com/tuhuynh27/go-ioc/examples/ioc-example-simple/message"
 	"github.com/tuhuynh27/go-ioc/examples/ioc-example-simple/notification"
-	"github.com/tuhuynh27/go-ioc/ioc"
+	"github.com/tuhuynh27/go-ioc/examples/ioc-example-simple/wire"
 )
 
 func main() {
-	container := ioc.NewContainer()
-
-	err := container.RegisterComponents(
-		&logger.StdoutLogger{},
-		&message.EmailService{},
-		&message.SMSService{},
-		&notification.NotificationService{},
-	)
-
+	container, err := wire.InitializeContainer()
 	if err != nil {
 		panic(err)
 	}
 
-	notificationService := container.Get("NotificationService").(*notification.NotificationService)
+	notificationService := container.Get("github.com/tuhuynh27/go-ioc/examples/ioc-example-simple/notification.NotificationService").(*notification.NotificationService)
 	notificationService.SendNotifications("Hello, World!")
 }
