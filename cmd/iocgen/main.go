769f77110b77fa46f6af7e182e921fa25fef83a6
Tu Huynh <tu.huynh@rakuten.com>
2024-11-28 11:36:00 +0800

    update main

--- cmd/iocgen/main.go
+++ cmd/iocgen/main.go
@@ -13,58 +13,58 @@ var (
 	rootCmd = &cobra.Command{
 		Use:   "ioc-generate",
 		Short: "Go IoC - Dependency Injection Code Generator",
-		Run:   func(cmd *cobra.Command, args []string) {},
+		Run: func(cmd *cobra.Command, args []string) {
+			if help {
+				cmd.Help()
+				return
+			}
+
+			// Convert to absolute path
+			absDir, err := filepath.Abs(dir)
+			if err != nil {
+				log.Fatalf("Error getting absolute path: %v", err)
+			}
+
+			log.Printf("Scanning directory: %s", absDir)
+
+			// Parse components
+			components, err := wire.ParseComponents(absDir)
+
+			if err != nil {
+				log.Fatalf("Error parsing components: %v", err)
+			}
+
+			if verbose {
+				for _, comp := range components {
+					log.Printf("Component: %s (package: %s)", comp.Name, comp.Package)
+					log.Printf("- Qualifier: %s", comp.Qualifier)
+					log.Printf("- Implements: %v", comp.Implements)
+					log.Printf("- Dependencies: %v", comp.Dependencies)
+				}
+			}
+
+			// Generate code
+			gen := wire.NewGenerator(components)
+			if err := gen.Generate(absDir); err != nil {
+				log.Fatalf("Error generating code: %v", err)
+			}
+
+			log.Printf("Successfully generated wire file: %s/%s", absDir, output)
+		},
 	}
 
 	dir, output   string
 	verbose, help bool
 )
 
-func init() {
-	rootCmd.InitDefaultHelpCmd()
-}
-
 func main() {
+	rootCmd.PersistentFlags().StringVarP(&dir, "dir", "d", ".", "Directory to scan for components")
+	rootCmd.PersistentFlags().StringVarP(&output, "output", "o", "wire/wire_gen.go", "Output file for generated code")
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose logging")
+	rootCmd.PersistentFlags().BoolVarP(&help, "help", "h", false, "Show help message")
+
 	printBanner()
-
-	rootCmd.PersistentFlags().StringVar(&dir, "dir", ".", "Directory to scan for components")
-	rootCmd.PersistentFlags().StringVar(&output, "output", "wire/wire_gen.go", "Output file for generated code")
-	rootCmd.PersistentFlags().BoolVar(&verbose, "verbose", false, "Enable verbose logging")
-	rootCmd.PersistentFlags().BoolVar(&help, "help", false, "Show help message")
-
 	rootCmd.Execute()
-
-	// Convert to absolute path
-	absDir, err := filepath.Abs(dir)
-	if err != nil {
-		log.Fatalf("Error getting absolute path: %v", err)
-	}
-
-	log.Printf("Scanning directory: %s", absDir)
-
-	// Parse components
-	components, err := wire.ParseComponents(absDir)
-
-	if err != nil {
-		log.Fatalf("Error parsing components: %v", err)
-	}
-
-	if verbose {
-		for _, comp := range components {
-			log.Printf("Component: %s (package: %s)", comp.Name, comp.Package)
-			log.Printf("- Qualifier: %s", comp.Qualifier)
-			log.Printf("- Implements: %v", comp.Implements)
-			log.Printf("- Dependencies: %v", comp.Dependencies)
-		}
-	}
-
-	// Generate code
-	gen := wire.NewGenerator(components)
-	if err := gen.Generate(absDir); err != nil {
-		log.Fatalf("Error generating code: %v", err)
-	}
-
-	log.Printf("Successfully generated wire file: %s/%s", absDir, output)
 }
 
 func printBanner() {
